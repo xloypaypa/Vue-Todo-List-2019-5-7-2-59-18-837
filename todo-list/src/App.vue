a1abf30f7f4442d463901a10a4087c64d37141de
xsu <xsu@thoughtworks.com>
2019-07-03 14:17:24 +0800

    can add new item to todo list.

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <h2>Vue To Do List</h2>
     <p>Simple Todo List with adding and filter by diff status.</p>
-
+    <label>
+      <input v-model="todoName"/>
+    </label>
+    <button v-on:click="addItem">Add</button>
     <TodoList v-bind:todoList="todoList"/>
   </div>
 </template>
@@ -13,8 +16,14 @@ import TodoList from "./components/TodoList";
 export default {
   name: 'app',
   components: {TodoList},
+  methods:{
+    addItem: function () {
+      this.todoList.push(this.todoName);
+    }
+  },
   data: function () {
     return {
+      todoName: "",
       todoList: ['a', 'b', 'c']
     }
   }
