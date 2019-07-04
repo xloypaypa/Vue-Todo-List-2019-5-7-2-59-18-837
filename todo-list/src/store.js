d450da2d64d456988c4cd0daf128f966225fcc1e
xsu <xsu@thoughtworks.com>
2019-07-04 11:10:29 +0800

    add getter and mutation for store.

--- todo-list/src/store.js
+++ todo-list/src/store.js
@@ -10,9 +10,18 @@ const state = {
 const store = new Vuex.Store({
     state,
     getters: {
+        getTodoList: (state) => () => {
+            return state.todoList;
+        }
     },
     mutations: {
-
+        addItem(state, todoItemWithoutIndex) {
+            const index = state.todoList.length;
+            Vue.set(state.todoList, index, {...todoItemWithoutIndex, index: index});
+        },
+        updateTodoItem(state, todoItem) {
+            Vue.set(state.todoList, index, todoItem);
+        },
     }
 });
 
