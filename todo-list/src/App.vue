ca18d1fabcb4c9d42917314192a7743765e7031c
suxin <xloypaypa@gmail.com>
2019-07-21 16:01:00 +0800

    clean up app

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,59 +1,20 @@
 <template>
     <div>
         <Hello v-if="!status"/>
-        <div id="app" v-if="status">
-            <h2>Vue To Do List</h2>
-            <p>Simple Todo List with adding and filter by diff status.</p>
-            <label>
-                <input v-model="todoName"/>
-            </label>
-            <button v-on:click="addItem">Add</button>
-            <TodoList v-bind:todoList="displayList" v-on:update-todo-item="updateTodoItem"/>
-            <button v-on:click="updateFilter(showAll)">ALL</button>
-            <button v-on:click="updateFilter(showActive)">Active</button>
-            <button v-on:click="updateFilter(showComplete)">Complete</button>
-        </div>
+        <TodoMain/>
     </div>
 </template>
 
 <script>
 
-    import TodoList from "./components/TodoList";
     import Hello from "./components/Hello";
+    import TodoMain from "./TodoMain"
 
     export default {
         name: 'app',
-        components: {Hello, TodoList},
-        methods: {
-            addItem: function () {
-                const index = this.todoList.length;
-                this.todoList.push({index: index, name: this.todoName, finished: false});
-                this.displayList = this.todoList.filter(this.displayFilter);
-                this.todoName = "";
-            },
-            updateTodoItem: function (event) {
-                this.todoList[event.index].name = event.name;
-                this.todoList[event.index].finished = event.finished;
-            },
-            showAll: function () {
-                return true;
-            },
-            showActive: function (todoItem) {
-                return !todoItem.finished;
-            },
-            showComplete: function (todoItem) {
-                return todoItem.finished;
-            },
-            updateFilter: function (filter) {
-                this.displayList = this.todoList.filter(filter);
-            }
-        },
+        components: {Hello, TodoMain},
         data: function () {
             return {
-                todoName: "",
-                todoList: [],
-                displayList: [],
-                displayFilter: this.showAll,
                 status: window.location.pathname !== '/hello'
             }
         }
---
+++ todo-list/src/TodoMain.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+        <h2>Vue To Do List</h2>
+        <p>Simple Todo List with adding and filter by diff status.</p>
+        <label>
+            <input v-model="todoName"/>
+        </label>
+        <button v-on:click="addItem">Add</button>
+        <TodoList v-bind:todoList="displayList" v-on:update-todo-item="updateTodoItem"/>
+        <button v-on:click="updateFilter(showAll)">ALL</button>
+        <button v-on:click="updateFilter(showActive)">Active</button>
+        <button v-on:click="updateFilter(showComplete)">Complete</button>
+    </div>
+</template>
+
+<script>
+
+    import TodoList from "./components/TodoList";
+
+    export default {
+        name: 'app',
+        components: {TodoList},
+        methods: {
+            addItem: function () {
+                const index = this.todoList.length;
+                this.todoList.push({index: index, name: this.todoName, finished: false});
+                this.displayList = this.todoList.filter(this.displayFilter);
+                this.todoName = "";
+            },
+            updateTodoItem: function (event) {
+                this.todoList[event.index].name = event.name;
+                this.todoList[event.index].finished = event.finished;
+            },
+            showAll: function () {
+                return true;
+            },
+            showActive: function (todoItem) {
+                return !todoItem.finished;
+            },
+            showComplete: function (todoItem) {
+                return todoItem.finished;
+            },
+            updateFilter: function (filter) {
+                this.displayList = this.todoList.filter(filter);
+            }
+        },
+        data: function () {
+            return {
+                todoName: "",
+                todoList: [],
+                displayList: [],
+                displayFilter: this.showAll,
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
